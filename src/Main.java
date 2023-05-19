982c3006ecdaab8be38a45fe6159418ce995e4d6
camil <camil@LAPTOP-B56TEV94>
2023-05-19 16:05:53 +0200

    Consegna Veririca con correzione Errori!

--- src/Main.java
+++ src/Main.java
@@ -7,3 +7,5 @@ public class Main
 		Frame A= new Frame();
 	}
 }
+// C'è qualche errore di forma e quindi scaricando il progetto non è eseguibile, ma il codice va bene! come detto in classe
+//Il pulsante "rimuovi" è stato implementato per cancellare il contenuto dell'ArrayList e il contenuto dei JText!!
